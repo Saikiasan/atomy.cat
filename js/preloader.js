3af631c06d5095da64d1f6e21b59d0f32f3b85f7
Ankit Saikia <112230331+Saikiasan@users.noreply.github.com>
2023-10-07 15:24:20 +0530

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -1,10 +1,9 @@
 export const preload = () => {
   if ($('.preloader').length) {
     $('.preloader').delay(1500).fadeOut('slow').promise().then(function () {
-      $(this).detach();
-      $('header').toggleClass('visually-hidden').then(
-        $('menu').addClass('animated slideInUp fast')
-      )
+      $(this).detach()
+      $('header').toggleClass('visually-hidden')
+      $('menu').addClass('animated slideInUp fast')
     })
   }
 }
