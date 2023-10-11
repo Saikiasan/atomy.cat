7a4a8e5ef14c7d58fc56498adfe0e5f5e6a90dfa
Ankit Saikia <112230331+Saikiasan@users.noreply.github.com>
2023-10-11 23:03:35 +0530

    minor

--- js/preloader.js
+++ js/preloader.js
@@ -2,7 +2,8 @@ export const preload = () => {
   if ($('.preloader').length) {
     $('.preloader').delay(1500).fadeOut('slow').promise().then(function () {
       $(this).detach()
-      $('header').toggleClass('visually-hidden')
+      // MENU
+      $('#menu-con').toggleClass('visually-hidden')
       $('menu').addClass('animated slideInUp fast')
     })
   }
