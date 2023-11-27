27a231d23d62e35c187036b70eb20a785838608e
Ankit Saikia <112230331+Saikiasan@users.noreply.github.com>
2023-11-27 18:59:46 +0530

    removing large data

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import {activeLink, theme, urlCrawler} from "./nav.min.js";
-import { preload } from "./preloader.min.js";
-// navigation()
-preload()
-// clicks()
-// urlCrawler()
-theme('.navbar-brand')
-activeLink()
